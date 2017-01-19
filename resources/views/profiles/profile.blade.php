9c42ac4af30bc913ccbc34089c0b364dbe8d3fb1
Nathan Queija <nathan@centesimo.com.br>
2017-01-19 15:51:16 -0200

    Commit inicial

---
+++ resources/views/profiles/profile.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts/app')
+
+@section('content')
+    <div class="container">
+        <div class="col-lg-4">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    Perfil de {{$user->name}}
+                </div>
+
+                <div class="panel-body">
+
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
